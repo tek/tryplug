5fe9cf40e6e091bd49fe8353166862e099d73b17
tek <torstenschmits@gmail.com>
2016-01-03 20:16:19 +0100

    add extra generator command for android project

--- src/plugin.scala
+++ src/plugin.scala
@@ -12,6 +12,8 @@ extends AutoPlugin
   object autoImport
   {
     def tryplugVersion = TrypKeys.tryplugVersion
+    def protifyVersion = TrypKeys.protifyVersion
+    def sdkVersion = TrypKeys.sdkVersion
   }
 }
 
@@ -75,42 +77,63 @@ extends AutoPlugin
     commands ++= List(genTryp, genTrypAndroid)
     )
 
-  val ptryp = "lazy val `project` = trypProjectBuild"
+  def ptryp(plug: String, buildHelper: String) = s"""enablePlugins($plug)
+lazy val `project` = $buildHelper"""
 
-  val trypver = """trypVersion := "83""""
+  val trypver = """trypVersion := "84-SNAPSHOT""""
 
-  val pptryp = """
-resolvers += Resolver.url(
+  def pptryp(dep: String) = s"""resolvers += Resolver.url(
   "bintray-tek-sbt",
   url("https://dl.bintray.com/tek/sbt-plugins")
 )(Resolver.ivyStylePatterns)
-addSbtPlugin("tryp.sbt" % "tryp-build" % "83")
-  """
+libraryDependencies += Defaults.sbtPluginExtra(
+  "tryp.sbt" % "tryp-$dep" % trypVersion.value,
+  (sbtBinaryVersion in update).value,
+  (scalaBinaryVersion in update).value
+)
+"""
 
-  val pppbuild = """
-import sbt._
+  val pppbuild = """import sbt._
 
 object P
 extends Plugin
 {
   val trypVersion = settingKey[String]("tryp version")
+}"""
+
+  def build(base: String, name: String, pkg: String = "tryp") =
+    s"""package $pkg
+
+import sbt._, Keys._
+
+object Build
+extends $base("$name")
+{
+  lazy val core = "core" !
 }
-  """
+"""
 
-  def genTryp = Command.command("gen-tryp") { state ⇒
-    val extracted = sbt.Project.extract(state)
-    import extracted._
-    val pro = get(baseDirectory) / "project"
-    val pp = pro / "project"
-    val ppp = pp / "project"
-    IO.createDirectory(pp)
-    IO.write(pro / "trypVersion.sbt", trypver)
-    IO.write(pro / "tryp.sbt", ptryp)
-    IO.write(pp / "trypVersion.sbt", trypver)
-    IO.write(pp / "tryp.sbt", pptryp)
-    IO.write(ppp / "build.scala", pppbuild)
-    state
-  }
+  def genCommand(cmd: String, dep: String, base: String, plug: String,
+    buildHelper: String) =
+    Command.command(s"gen-$cmd") { state ⇒
+      val extracted = sbt.Project.extract(state)
+      import extracted._
+      val pro = get(baseDirectory) / "project"
+      val pp = pro / "project"
+      val ppp = pp / "project"
+      IO.createDirectory(pp)
+      IO.write(pro / "trypVersion.sbt", trypver)
+      IO.write(pro / "tryp.sbt", ptryp(plug, buildHelper))
+      IO.write(pp / "trypVersion.sbt", trypver)
+      IO.write(pp / "tryp.sbt", pptryp(dep))
+      IO.write(ppp / "build.scala", pppbuild)
+      IO.write(pro / "build.scala", build(base, get(name)))
+      state
+    }
 
-  def genTrypAndroid = genTryp
+  def genTryp = genCommand("tryp", "build", "MultiBuild", "TrypBuildPlugin",
+    "trypProjectBuild")
+
+  def genTrypAndroid = genCommand("tryp-android", "android",
+    "AndroidBuild", "TrypAndroidBuildPlugin", "trypAndroidProjectBuild")
 }

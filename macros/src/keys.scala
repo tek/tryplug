13cee807343c1f27ea9d51aeeed83303e5d065a4
Torsten Schmits <torstenschmits@gmail.com>
2017-03-13 09:50:45 +0100

    formatting

--- macros/src/keys.scala
+++ macros/src/keys.scala
@@ -11,8 +11,7 @@ object TrypKeys
   val sdkVersion = settingKey[String]("android-sdk-plugin version") in Tryp
   val protifyVersion = settingKey[String]("protify version") in Tryp
   val coursierVersion = settingKey[String]("coursier version") in Tryp
-  val useCoursier = settingKey[Boolean]("activate coursier in this project")
-    .in(Tryp)
+  val useCoursier = settingKey[Boolean]("activate coursier in this project") in Tryp
 }
 
 import TrypKeys.Tryp
@@ -20,24 +19,13 @@ import TrypKeys.Tryp
 object VersionUpdateKeys
 {
   val versions = settingKey[Seq[PluginSpec]]("auto-updated plugins") in Tryp
-  val projectDir =
-    settingKey[File]("project base dirs into which to write versions") in Tryp
+  val projectDir = settingKey[File]("project base dirs into which to write versions") in Tryp
   val updateVersions = taskKey[Unit]("update plugin dep versions") in Tryp
-  val autoUpdateVersions =
-    settingKey[Boolean]("update plugin versions when updating dependencies")
-      .in(Tryp)
-  val versionUpdater =
-    settingKey[Logger => Versions](
-      "Versions instance used for updating plugins") in Tryp
-  val updateAllPlugins =
-    settingKey[Boolean]("update all versions by default") in Tryp
-  val updatePluginsInclude =
-    settingKey[List[String]]("which plugins' versions to update") in Tryp
-  val updatePluginsExclude =
-    settingKey[List[String]]("which plugins' versions not to update") in Tryp
-  val versionDirMap =
-    settingKey[Map[String, List[File]]](
-      "settingKey -> directories for version updates") in Tryp
-  val handlePrefixMap =
-    settingKey[Map[File, String]]("dir -> prefix for version updates") in Tryp
+  val autoUpdateVersions = settingKey[Boolean]("update plugin versions when updating dependencies") in Tryp
+  val versionUpdater = settingKey[Logger => Versions]("Versions instance used for updating plugins") in Tryp
+  val updateAllPlugins = settingKey[Boolean]("update all versions by default") in Tryp
+  val updatePluginsInclude = settingKey[List[String]]("which plugins' versions to update") in Tryp
+  val updatePluginsExclude = settingKey[List[String]]("which plugins' versions not to update") in Tryp
+  val versionDirMap = settingKey[Map[String, List[File]]]("settingKey -> directories for version updates") in Tryp
+  val handlePrefixMap = settingKey[Map[File, String]]("dir -> prefix for version updates") in Tryp
 }
